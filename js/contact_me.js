cb9fb9a587460f001c880099d4436ff6a53424c3
ibly <ibly.solutions@gmail.com>
2015-01-07 12:30:41 +0000

    mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/info@iblysolutions.com",
+                url: "http://formspree.io/info@iblysolutions.com",
                 type: "POST",
                 data: {
                     name: name,
